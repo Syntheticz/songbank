98efcf46ec2de34a7506b23b16de39f25c816f24
Syntheticz <guiangphilip08@gmail.com>
2023-08-23 10:53:23 +0800

    Added Graphql functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+'use client'
 import React from 'react'
+import { gql } from '@apollo/client';
+export const dynamic = "force-dynamic";
+
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+
+const query = gql`query {
+  launchLatest {
+    mission_name
+  }
+}`
+
 
 export default function Home() {
+
+  const {data} = useSuspenseQuery(query);
+
+  function handleOnClick(e : React.MouseEvent<HTMLButtonElement, MouseEvent>){
+    console.log(data)
+  }
+
   return (
     <div className="w-4 h-4 bg-cyan-100">
-      
+      <button onClick={(e) => {handleOnClick(e)}} className='bg-slate-500 hover:bg-sky-400 active:bg-slate-900'>Test</button>
     </div>
   )
 }
